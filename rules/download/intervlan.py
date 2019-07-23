921e11af329d3406b876f4de5a24d16ab747d0bd
Anthony Ruhier <anthony.ruhier@gmail.com>
2019-07-23 02:59:29 +0200

    Back to PFIFO for intervlan
    Normally enough bandwidth to not care, and get better routing
    performance

--- rules/download/intervlan.py
+++ rules/download/intervlan.py
@@ -1,18 +1,15 @@
-from pyqos.algorithms.htb import HTBFilterFQCodel
+from pyqos.algorithms.htb import HTBFilterPFIFO
 
 from rules import app
 from rules.qos_formulas import burst_formula
 
 
-class InterVlan(HTBFilterFQCodel):
+class InterVlan(HTBFilterPFIFO):
     """
     Intervlan need to be fast
     """
     classid = "1:15"
     rate = (100, )
     burst = (burst_formula, [], {"post_compute": lambda x: x*3})
-    limit = burst
-    interval = 10
-    codel_quantum = 1514
     prio = 0
     mark = 0xa
