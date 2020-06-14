3b028f6834901feed1da41fedb99fcea6bd7f009
Anthony Ruhier <anthony.ruhier@gmail.com>
2020-06-14 22:30:04 +0200

    Change the burst formulas to avoid overflow
    burst and cburst overflow in iproute2

--- rules/qos_formulas.py
+++ rules/qos_formulas.py
@@ -4,10 +4,14 @@ def burst_formula(obj, post_compute=None):
 
     :param post_compute: lambda to apply on the computed burst
     """
-    burst = 0.5 * obj.rate/8
+    burst = 0.2 * obj.rate / 8
 
     if post_compute:
-        return post_compute(burst)
+        burst = post_compute(burst)
+
+    # Avoid overflow.
+    if burst > 2**22:
+        return 2**22 - 1
     return burst
 
 
@@ -17,8 +21,12 @@ def cburst_formula(obj, post_compute=None):
 
     :param post_compute: lambda to apply on the computed cburst
     """
-    cburst = 1.5 * obj.rate/8 + obj.burst
+    cburst = 0.5 * obj.rate / 8 + obj.burst
 
     if post_compute:
-        return post_compute(cburst)
+        cburst = post_compute(cburst)
+
+    # Avoid overflow.
+    if cburst > 2**22:
+        return 2**22 - 1
     return cburst
